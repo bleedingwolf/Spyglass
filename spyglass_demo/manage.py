1ab9c84a3e27fecc4feb0ddb79654e755a80c93a
Justin Voss <justin@bleedingwolf.com>
2010-07-01 19:18:49 -0500

    Separated the app from the project.

--- spyglass_demo/manage.py
+++ spyglass_demo/manage.py
@@ -7,5 +7,9 @@ except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
+import sys
+import os
+sys.path.append(os.path.dirname(settings.PROJECT_DIR))
+
 if __name__ == "__main__":
     execute_manager(settings)
